8ca2c5faab079fcb5e7e11f8ea835b91b5757183
Noah Garrett <67662284+noahgarrett@users.noreply.github.com>
2023-07-02 14:28:03 -0400

    no argument functions and local variables work

--- BlazeGo/vm/vm_test.go
+++ BlazeGo/vm/vm_test.go
@@ -247,6 +247,18 @@ func TestFirstClassFunctions(t *testing.T) {
 					   returnsOneReturner()();`,
 			expected: 1,
 		},
+		{
+			input: `
+			  let returnsOneReturner = fn() {
+				let returnsOne = fn() {
+				  1;
+				};
+				returnsOne;
+			  };
+			  returnsOneReturner()();
+			`,
+			expected: 1,
+		},
 	}
 	runVmTests(t, tests)
 }
